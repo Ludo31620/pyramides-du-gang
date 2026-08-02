a274a1292bed619385c5da3802d63892ba86a3b9
Ludovic Bataille <ludo3005@hotmail.fr>
2026-08-02 18:17:22 +0200

    Prepare production deployment

--- server.ts
+++ server.ts
@@ -147,13 +147,14 @@ const dev =
   "production";
 
 const hostname =
-  process.env.HOSTNAME ??
   "0.0.0.0";
 
 const port =
   Number(
     process.env.PORT
   ) || 3000;
+
+
 
 const app = next({
   dev,
